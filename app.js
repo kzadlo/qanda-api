60f90eaaaee517bc2602c9cf9b0bb4171e476292
Krystian Żądło <krystianzadlo@fejm.pl>
2020-07-13 08:23:45 +0200

    chore: require body parser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 const questionRoutes = require('./api/routes/questions');
 const answerRoutes = require('./api/routes/answers');
