1214edb5f19b750faca396390db7b5b1961fe99b
Krystian Żądło <krystianzadlo@fejm.pl>
2020-07-13 08:01:35 +0200

    feature: add basic error handling

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 const questionRoutes = require('./api/routes/questions');
 const answerRoutes = require('./api/routes/answers');
@@ -7,9 +10,18 @@ const answerRoutes = require('./api/routes/answers');
 app.use('/questions', questionRoutes);
 app.use('/answers', answerRoutes);
 
-app.use('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Server and app works!'
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
